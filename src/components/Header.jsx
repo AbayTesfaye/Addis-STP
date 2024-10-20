8cc65920aa0c1d31a341215a630978ae78e91aae
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-20 06:49:04 -0700

    Add Heading component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="header">
+      <h1>Songs List</h1>
+      <div>
+        <input type="text" placeholder="Search" />
+        <button>Upload</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
